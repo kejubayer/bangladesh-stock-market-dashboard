459f90788bbf5fa2a8e8dcc130802ecade224a9d
kejubayer <khondokereftakharjubayer@gmail.com>
2026-03-31 18:29:03 +0600

    footer name

--- resources/views/layouts/partials/frontend/_footer.blade.php
+++ resources/views/layouts/partials/frontend/_footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="bg-dark text-white py-3 mt-auto text-center">
-    &copy; {{ date('Y') }} {{config('app.name')}} <a href="">(kejubayer)</a>. All rights reserved.
+    &copy; {{ date('Y') }} {{config('app.name')}} <a href="https://github.com/kejubayer" target="_blank">(kejubayer)</a>. All rights reserved.
 </footer>
